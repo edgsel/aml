b7481dccd06d9b1d8fbdc2da17d42b922256d0cc
edgsel <edgar.selihov@gmail.com>
2023-10-28 17:18:55 +0300

    LHV-002 - Add SanctionedPersonRepository

---
+++ src/main/java/ee/lhv/aml/repository/SanctionedPersonRepository.java
@@ -0,0 +1,8 @@
+package ee.lhv.aml.repository;
+
+import ee.lhv.aml.entity.SanctionedPerson;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SanctionedPersonRepository extends CrudRepository<SanctionedPerson, Long> {
+
+}
