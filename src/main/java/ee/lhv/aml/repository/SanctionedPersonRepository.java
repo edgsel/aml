b67201808fe9c921c0da3471bc8f82686b07e31e
edgsel <edgar.selihov@gmail.com>
2023-10-28 17:19:13 +0300

    LHV-002 - add @Repository annotation

--- src/main/java/ee/lhv/aml/repository/SanctionedPersonRepository.java
+++ src/main/java/ee/lhv/aml/repository/SanctionedPersonRepository.java
@@ -2,7 +2,9 @@ package ee.lhv.aml.repository;
 
 import ee.lhv.aml.entity.SanctionedPerson;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SanctionedPersonRepository extends CrudRepository<SanctionedPerson, Long> {
 
 }
