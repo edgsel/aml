3c505d342f0360edd06090476dd4832e4f222530
edgsel <edgar.selihov@gmail.com>
2023-10-29 20:30:34 +0200

    LHV-002 - add sanctionedPersonService usage to SanctionedPersonController

--- src/main/java/ee/lhv/aml/rest/controller/SanctionedPersonController.java
+++ src/main/java/ee/lhv/aml/rest/controller/SanctionedPersonController.java
@@ -1,7 +1,9 @@
 package ee.lhv.aml.rest.controller;
 
 import ee.lhv.aml.rest.dto.request.SanctionedPersonCheckRequest;
+import ee.lhv.aml.service.SanctionedPersonService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,13 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.OK;
 
-
 @Validated
 @RestController
+@RequiredArgsConstructor
 public class SanctionedPersonController extends AmlApiController {
+
+    private final SanctionedPersonService sanctionedPersonService;
 
     @PostMapping("/sanctioned-person/checking")
     public ResponseEntity<Boolean> checkPerson(@Valid @RequestBody SanctionedPersonCheckRequest request) {
-        return new ResponseEntity<>(true, OK);
+        boolean result = sanctionedPersonService.isNameInSanctionedPersons(request.getSl(), request.getUser());
+
+        return new ResponseEntity<>(result, OK);
     }
 }
