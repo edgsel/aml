3275272b82643320a808453a432006c92434a88e
edgsel <edgar.selihov@gmail.com>
2023-10-29 20:27:32 +0200

    LHV-002 - merge name/sl tokens to check all similarities

--- src/main/java/ee/lhv/aml/service/SanctionedPersonService.java
+++ src/main/java/ee/lhv/aml/service/SanctionedPersonService.java
@@ -5,8 +5,10 @@ import ee.lhv.aml.persistance.SanctionPersonEntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static ee.lhv.aml.util.JaroWinklerUtil.isSimilarEnough;
 import static ee.lhv.aml.util.NamePreprocessorUtil.preprocessName;
@@ -20,10 +22,20 @@ public class SanctionedPersonService {
     public boolean isNameInSanctionedPersons(String sl, String user) {
         Set<String> slTokens = preprocessName(sl);
         Set<String> userTokens = preprocessName(user);
+        Set<String> mergedSlAndUserTokens = mergeNameTokenSets(slTokens, userTokens);
 
         List<SanctionedPerson> queryResults = sanctionPersonEntityManager.findSanctionedPersons(slTokens, userTokens);
 
         return queryResults.stream()
-            .anyMatch(result -> isSimilarEnough(sl, result) || isSimilarEnough(user, result));
+            .anyMatch(result -> mergedSlAndUserTokens.stream().anyMatch(token -> isSimilarEnough(token, result)));
+    }
+
+    @SafeVarargs
+    private Set<String> mergeNameTokenSets(Set<String>... sets) {
+        Set<String> merged = new HashSet<>();
+
+        Stream.of(sets).forEach(merged::addAll);
+
+        return merged;
     }
 }
