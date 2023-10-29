1d57af3c8acd975ea3ad7606e61029163ddd9af2
edgsel <edgar.selihov@gmail.com>
2023-10-29 20:26:40 +0200

    LHV-002 - handle npe for missing data in name columns

--- src/main/java/ee/lhv/aml/util/JaroWinklerUtil.java
+++ src/main/java/ee/lhv/aml/util/JaroWinklerUtil.java
@@ -3,6 +3,8 @@ package ee.lhv.aml.util;
 import ee.lhv.aml.entity.SanctionedPerson;
 import org.apache.commons.text.similarity.JaroWinklerDistance;
 
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
 public class JaroWinklerUtil {
 
     private static final double JARO_WINKLER_THRESHOLD = 0.85;
@@ -10,11 +12,19 @@ public class JaroWinklerUtil {
     private static final JaroWinklerDistance jaroWinklerDistance = new JaroWinklerDistance();
 
     public static boolean isSimilarEnough(String name, SanctionedPerson person) {
-        return jaroWinklerDistance.apply(name, person.getName6()) > JARO_WINKLER_THRESHOLD ||
-            jaroWinklerDistance.apply(name, person.getName1()) > JARO_WINKLER_THRESHOLD ||
-            jaroWinklerDistance.apply(name, person.getName2()) > JARO_WINKLER_THRESHOLD ||
-            jaroWinklerDistance.apply(name, person.getName3()) > JARO_WINKLER_THRESHOLD ||
-            jaroWinklerDistance.apply(name, person.getName4()) > JARO_WINKLER_THRESHOLD ||
-            jaroWinklerDistance.apply(name, person.getName5()) > JARO_WINKLER_THRESHOLD;
+        return checkWinklerDistance(name, person.getName6()) ||
+            checkWinklerDistance(name, person.getName1()) ||
+            checkWinklerDistance(name, person.getName2()) ||
+            checkWinklerDistance(name, person.getName3()) ||
+            checkWinklerDistance(name, person.getName4()) ||
+            checkWinklerDistance(name, person.getName5());
+    }
+
+    private static boolean checkWinklerDistance(String nameFromRequest, String nameFromDb) {
+        if (isEmpty(nameFromRequest) || isEmpty(nameFromDb)) {
+            return false;
+        }
+
+        return jaroWinklerDistance.apply(nameFromRequest, nameFromDb) > JARO_WINKLER_THRESHOLD;
     }
 }
