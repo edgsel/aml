966dab82ecdd1b78f7f9de5bc00ffc17e77a5d2a
edgsel <edgar.selihov@gmail.com>
2023-10-30 19:39:17 +0200

    LHV-002 - fix mapstruct compile issue, add global exception handler

---
+++ src/main/java/ee/lhv/aml/handler/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package ee.lhv.aml.handler;
+
+import ee.lhv.aml.rest.dto.response.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final String INTERNAL_SERVER_ERROR_MSG = "Server made boo boo, please try later";
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleInvalidEnumExceptions(Exception ex) {
+        return new ResponseEntity<>(new ErrorResponse(INTERNAL_SERVER_ERROR_MSG), INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ src/main/java/ee/lhv/aml/rest/dto/response/ErrorResponse.java
@@ -0,0 +1,3 @@
+package ee.lhv.aml.rest.dto.response;
+
+public record ErrorResponse(String errorMessage) { }
