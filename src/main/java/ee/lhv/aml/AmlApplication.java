1d27f6accc750fd35253e4773a74f5a9c468d4b7
edgsel <edgar.selihov@gmail.com>
2023-11-01 02:27:55 +0200

    LHV-006 - add swagger api docs

--- src/main/java/ee/lhv/aml/AmlApplication.java
+++ src/main/java/ee/lhv/aml/AmlApplication.java
@@ -1,11 +1,20 @@
 package ee.lhv.aml;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@OpenAPIDefinition(
+	info = @Info(
+		title = "LHV AML API",
+		version = "1.0",
+		description = "Service to validate if the person is sanctioned by EU banks"
+	)
+)
 public class AmlApplication {
 
 	public static void main(String[] args) {
--- src/main/java/ee/lhv/aml/rest/controller/SanctionedPersonController.java
+++ src/main/java/ee/lhv/aml/rest/controller/SanctionedPersonController.java
@@ -6,6 +6,11 @@ import ee.lhv.aml.rest.dto.request.SanctionedPersonCheckRequest;
 import ee.lhv.aml.rest.dto.response.SanctionedPersonResponse;
 import ee.lhv.aml.rest.mapper.SanctionedPersonMapper;
 import ee.lhv.aml.service.SanctionedPersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -31,6 +36,12 @@ public class SanctionedPersonController extends AmlApiController {
 
     private final SanctionedPersonMapper sanctionedPersonMapper;
 
+    @Operation(summary = "Check if person is sanctioned")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Returns list sanctioned person(s) or empty list",
+            content = { @Content(mediaType = "application/json",
+                schema = @Schema(implementation = SanctionedPerson.class)) })
+    })
     @PostMapping(value = "/sanctioned-person/check", produces = "application/json")
     public ResponseEntity<List<SanctionedPersonResponse>> checkPerson(@Valid @RequestBody SanctionedPersonCheckRequest request) {
         List<SanctionedPerson> results = sanctionedPersonService.isNameInSanctionedPersons(
@@ -41,6 +52,16 @@ public class SanctionedPersonController extends AmlApiController {
         return new ResponseEntity<>(sanctionedPersonMapper.mapToSanctionedPersonsResponse(results), OK);
     }
 
+    @Operation(summary = "Add a new sanctioned person")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+        description = "NB! Use dd/MM/yyyy for date fields. Required fields: name6, groupType, aliasType, regime, groupId")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "New sanctioned person is added",
+            content = { @Content(mediaType = "application/json",
+                schema = @Schema(implementation = SanctionedPerson.class)) }),
+        @ApiResponse(responseCode = "400", description = "Request validation failed",
+            content = @Content)
+    })
     @PostMapping(value = "/sanctioned-person", produces = "application/json")
     public ResponseEntity<SanctionedPersonResponse> addNewPerson(@Valid @RequestBody SanctionedPersonRequest request) {
         SanctionedPerson newPerson = sanctionedPersonMapper.mapToSanctionedPersonEntity(request);
@@ -49,6 +70,17 @@ public class SanctionedPersonController extends AmlApiController {
         return new ResponseEntity<>(sanctionedPersonMapper.mapToSanctionedPersonResponse(result), CREATED);
     }
 
+    @Operation(summary = "Update an existing sanction person by its ID")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+        description = "NB! Use dd/MM/yyyy for date fields. Required fields: name6, groupType, aliasType, regime, groupId")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Sanctioned person updated",
+            content = { @Content(mediaType = "application/json",
+                schema = @Schema(implementation = SanctionedPerson.class)) }),
+        @ApiResponse(responseCode = "400", description = "Request validation failed",
+            content = @Content),
+        @ApiResponse(responseCode = "404", description = "Sanctioned person not found",
+            content = @Content) })
     @PutMapping(value = "/sanctioned-person/{personId}", produces = "application/json")
     public ResponseEntity<SanctionedPersonResponse> updatePerson(
         @PathVariable Long personId,
@@ -60,6 +92,13 @@ public class SanctionedPersonController extends AmlApiController {
         return new ResponseEntity<>(sanctionedPersonMapper.mapToSanctionedPersonResponse(result), OK);
     }
 
+    @Operation(summary = "Delete an existing sanction person by its ID")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Sanctioned person deleted",
+            content = { @Content(mediaType = "application/json",
+                schema = @Schema(implementation = Boolean.class)) }),
+        @ApiResponse(responseCode = "404", description = "Sanctioned person not found",
+            content = @Content) })
     @DeleteMapping(value = "/sanctioned-person/{personId}", produces = "application/json")
     public ResponseEntity<Boolean> deletePerson(@PathVariable Long personId) {
         return new ResponseEntity<>(sanctionedPersonService.deleteSanctionedPerson(personId), OK);
